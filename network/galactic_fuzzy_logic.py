beb7811fa29f3399f08f4dc7350d6721ee223a8d
KOSASIH <kosasihg88@gmail.com>
2024-10-11 20:56:33 +0700

    Update galactic_fuzzy_logic.py

--- network/galactic_fuzzy_logic.py
+++ network/galactic_fuzzy_logic.py
@@ -1,26 +1,47 @@
-import skfuzzy
-from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.backends import default_backend
+import numpy as np
+from skfuzzy import control as ctrl
+from skfuzzy import fuzzyvariable as fuzz
 
 class GalacticFuzzyLogic:
-    def __init__(self, node_id, private_key, network_config):
-        self.node_id = node_id
-        self.private_key = private_key
-        self.network_config = network_config
-        self.fuzzy_system = skfuzzy.control.System()
+    def __init__(self):
+        self.distance = ctrl.Antecedent(np.arange(0, 101, 1), 'distance')
+        self.velocity = ctrl.Antecedent(np.arange(0, 201, 1), 'velocity')
+        self.galaxy_type = ctrl.Consequent(np.arange(0, 3, 1), 'galaxy_type')
 
-    def define_fuzzy_variables(self):
-        # Define fuzzy variables using fuzzy logic
-        self.fuzzy_system.add_input_variable(skfuzzy.control.InputVariable('input', 0, 100))
-        self.fuzzy_system.add_output_variable(skfuzzy.control.OutputVariable('output', 0, 100))
+        self.distance.automf(3)
+        self.velocity.automf(3)
+        self.galaxy_type.automf(3)
 
-    def define_fuzzy_rules(self):
-        # Define fuzzy rules using fuzzy logic
-        self.fuzzy_system.add_rule(skfuzzy.control.Rule(self .fuzzy_system.input_variables[0]['input'] == 'low', self.fuzzy_system.output_variables[0]['output'] == 'low'))
-        self.fuzzy_system.add_rule(skfuzzy.control.Rule(self.fuzzy_system.input_variables[0]['input'] == 'high', self.fuzzy_system.output_variables[0]['output'] == 'high'))
+        self.rule1 = ctrl.Rule(self.distance['poor'] & self.velocity['poor'], self.galaxy_type['Spiral'])
+        self.rule2 = ctrl.Rule(self.distance['avg'] & self.velocity['avg'], self.galaxy_type['Elliptical'])
+        self.rule3 = ctrl.Rule(self.distance['good'] & self.velocity['good'], self.galaxy_type['Irregular'])
 
-    def evaluate_fuzzy_system(self, input_value):
-        # Evaluate the fuzzy system using fuzzy logic
-        output_value = self.fuzzy_system.evaluate(input_value)
-        return output_value
+        self.galaxy_type_ctrl = ctrl.ControlSystem([self.rule1, self.rule2, self.rule3])
+        self.galaxy_type_sim = ctrl.ControlSystemSimulation(self.galaxy_type_ctrl)
+
+    def evaluate(self, distance, velocity):
+        self.galaxy_type_sim.input['distance'] = distance
+        self.galaxy_type_sim.input['velocity'] = velocity
+        self.galaxy_type_sim.compute()
+        return self.galaxy_type_sim.output['galaxy_type']
+
+def main():
+    gfl = GalacticFuzzyLogic()
+
+    distance = 50
+    velocity = 100
+    galaxy_type = gfl.evaluate(distance, velocity)
+    print("Galaxy Type:", galaxy_type)
+
+    distance = 80
+    velocity = 150
+    galaxy_type = gfl.evaluate(distance, velocity)
+    print("Galaxy Type:", galaxy_type)
+
+    distance = 20
+    velocity = 50
+    galaxy_type = gfl.evaluate(distance, velocity)
+    print("Galaxy Type:", galaxy_type)
+
+if __name__ == "__main__":
+    main()
