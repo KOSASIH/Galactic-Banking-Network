b3a11108bb2684acfcb3ebd815cbb44f3e962354
KOSASIH <kosasihg88@gmail.com>
2024-10-11 21:01:06 +0700

    Update galactic_neural_network_architecture.py

--- network/galactic_neural_network_architecture.py
+++ network/galactic_neural_network_architecture.py
@@ -1,27 +1,53 @@
-import tensorflow
-from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.backends import default_backend
+import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Dense, Dropout, concatenate
 
 class GalacticNeuralNetworkArchitecture:
-    def __init__(self, node_id, private_key, network_config):
-        self.node_id = node_id
-        self.private_key = private_key
-        self.network_config = network_config
-        self.neural_network = tensorflow.keras.models.Sequential()
+    def __init__(self, input_shape, num_classes):
+        self.input_shape = input_shape
+        self.num_classes = num_classes
 
-    def design_neural_network(self):
-        # Design a neural network architecture using machine learning algorithms
-        self.neural_network.add(tensorflow.keras.layers.Dense(64, activation='relu', input_shape=(784,)))
-        self.neural_network.add(tensorflow.keras.layers.Dense(32, activation='relu'))
-        self.neural_network.add(tensorflow.keras.layers.Dense(10, activation='softmax'))
+    def create_model(self):
+        # Input layers
+        distance_input = Input(shape=(self.input_shape[0],), name='distance_input')
+        velocity_input = Input(shape=(self.input_shape[1],), name='velocity_input')
+        galaxy_type_input = Input(shape=(self.input_shape[2],), name='galaxy_type_input')
 
-    def train_neural_network(self):
-        # Train the neural network using machine learning algorithms
-        self.neural_network.compile(optimizer='adam', loss='mean_squared_error')
-        self.neural_network.fit(X_train, y_train, epochs=10)
+        # Distance branch
+        x_distance = Dense(64, activation='relu')(distance_input)
+        x_distance = Dropout(0.2)(x_distance)
+        x_distance = Dense(32, activation='relu')(x_distance)
 
-    def use_neural_network(self, input_data):
-        # Use the neural network to make predictions or classify data
-        output_data = self.neural_network.predict(input_data)
-        return output_data
+        # Velocity branch
+        x_velocity = Dense(64, activation='relu')(velocity_input)
+        x_velocity = Dropout(0.2)(x_velocity)
+        x_velocity = Dense(32, activation='relu')(x_velocity)
+
+        # Galaxy type branch
+        x_galaxy_type = Dense(64, activation='relu')(galaxy_type_input)
+        x_galaxy_type = Dropout(0.2)(x_galaxy_type)
+        x_galaxy_type = Dense(32, activation='relu')(x_galaxy_type)
+
+        # Merge branches
+        x = concatenate([x_distance, x_velocity, x_galaxy_type])
+
+        # Output layer
+        x = Dense(self.num_classes, activation='softmax')(x)
+
+        # Create model
+        model = Model(inputs=[distance_input, velocity_input, galaxy_type_input], outputs=x)
+
+        # Compile model
+        model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+        return model
+
+def main():
+    input_shape = (1, 1, 1)  # distance, velocity, galaxy type
+    num_classes = 3  # Spiral, Elliptical, Irregular
+    gnn = GalacticNeuralNetworkArchitecture(input_shape, num_classes)
+    model = gnn.create_model()
+    print(model.summary())
+
+if __name__ == "__main__":
+    main()
