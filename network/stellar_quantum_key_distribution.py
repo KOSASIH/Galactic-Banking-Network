88ce5ef81f9542016f14d06b2a9cfe53e4fc0c12
KOSASIH <kosasihg88@gmail.com>
2024-10-12 09:01:23 +0700

    Update stellar_quantum_key_distribution.py

--- network/stellar_quantum_key_distribution.py
+++ network/stellar_quantum_key_distribution.py
@@ -1,35 +1,120 @@
-import qiskit
+import os
+import hashlib
+import json
+from stellar_sdk import Server, Keypair, TransactionBuilder, Network
+from stellar_sdk.horizon import Horizon
+import pandas as pd
+import numpy as np
+from qiskit import QuantumCircuit, execute, Aer
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.backends import default_backend
 
 class StellarQuantumKeyDistribution:
-    def __init__(self, node_id, private_key, network_config):
-        self.node_id = node_id
-        self.private_key = private_key
-        self.network_config = network_config
-        self.quantum_key = None
+  def __init__(self, network_passphrase, horizon_url):
+    self.network_passphrase = network_passphrase
+    self.horizon_url = horizon_url
+    self.server = Server(horizon_url)
+    self.horizon = Horizon(horizon_url)
 
-    def generate_quantum_key(self):
-        # Generate a quantum key using a quantum computer
-        quantum_circuit = qiskit.QuantumCircuit(2)
-        quantum_circuit.h(0)
-        quantum_circuit.cx(0, 1)
-        quantum_circuit.measure([0, 1], [0, 1])
-        quantum_key = qiskit.execute(quantum_circuit, backend='qasm_simulator').result().get_counts()
-        self.quantum_key = quantum_key
+  def create_keypair(self):
+    keypair = Keypair.random()
+    return keypair
 
-    def distribute_quantum_key(self):
-        # Distribute the quantum key to other nodes in the network
-        for node in self.network_config['nodes']:
-            if node['node_id'] != self.node_id:
-                # Use quantum teleportation to send the quantum key to the node
-                quantum_teleportation(self.quantum_key, node['node_id'])
+  def create_account(self, keypair):
+    account = self.server.load_account(keypair.public_key)
+    if not account:
+      transaction = TransactionBuilder(
+        source_account=keypair.public_key,
+        network_passphrase=self.network_passphrase,
+        base_fee=100
+      ).append_create_account_op(
+        destination=keypair.public_key,
+        starting_balance="1000"
+      ).build()
+      transaction.sign(keypair)
+      response = self.server.submit_transaction(transaction)
+      print(f"Account created: {response}")
 
-    def verify_quantum_key(self):
-        # Verify the integrity of the quantum key
-        # Check for any errors or tampering
-        if verify_quantum_key(self.quantum_key):
-            return True
-        else:
-            return False
+  def send_asset(self, source_keypair, destination_keypair, asset_code, amount):
+    transaction = TransactionBuilder(
+      source_account=source_keypair.public_key,
+      network_passphrase=self.network_passphrase,
+      base_fee=100
+    ).append_payment_op(
+      destination=destination_keypair.public_key,
+      asset_code=asset_code,
+      amount=amount
+    ).build()
+    transaction.sign(source_keypair)
+    response = self.server.submit_transaction(transaction)
+    print(f"Asset sent: {response}")
+
+  def get_account_balance(self, keypair):
+    account = self.server.load_account(keypair.public_key)
+    balance = account.balances[0].balance
+    print(f"Account balance: {balance}")
+
+  def generate_quantum_key(self):
+    # Generate a quantum key using Qiskit
+    qc = QuantumCircuit(2)
+    qc.h(0)
+    qc.cx(0, 1)
+    qc.measure_all()
+    backend = Aer.get_backend('qasm_simulator')
+    job = execute(qc, backend, shots=1024)
+    result = job.result()
+    counts = result.get_counts(qc)
+    key = max(counts, key=counts.get)
+    return key
+
+  def generate_classical_key(self):
+    # Generate a classical key using cryptography library
+    key = rsa.generate_private_key(
+      public_exponent=65537,
+      key_size=2048,
+      backend=default_backend()
+    )
+    return key
+
+  def distribute_key(self, source_keypair, destination_keypair, key):
+    # Distribute the key using the Stellar network
+    transaction = TransactionBuilder(
+      source_account=source_keypair.public_key,
+      network_passphrase=self.network_passphrase,
+      base_fee=100
+    ).append_manage_data_op(
+      source_account=source_keypair.public_key,
+      data={b"key": key}
+    ).build()
+    transaction.sign(source_keypair)
+    response = self.server.submit_transaction(transaction)
+    print(f"Key distributed: {response}")
+
+  def retrieve_key(self, keypair):
+    # Retrieve the key from the Stellar network
+    account = self.server.load_account(keypair.public_key)
+    data = account.data
+    key = data[b"key"]
+    return key
+
+# Example usage:
+if __name__ == '__main__':
+  network_passphrase = "Test SDF Network ; September 2015"
+  horizon_url = "https://horizon-testnet.stellar.org"
+
+  stellar_quantum_key_distribution = StellarQuantumKeyDistribution(network_passphrase, horizon_url)
+
+  source_keypair = stellar_quantum_key_distribution.create_keypair()
+  destination_keypair = stellar_quantum_key_distribution.create_keypair()
+
+  stellar_quantum_key_distribution.create_account(source_keypair)
+  stellar_quantum_key_distribution.create_account(destination_keypair)
+
+  quantum_key = stellar_quantum_key_distribution.generate_quantum_key()
+  classical_key = stellar_quantum_key_distribution.generate_classical_key()
+
+  stellar_quantum_key_distribution.distribute_key(source_keypair, destination_keypair, quantum_key)
+  retrieved_key = stellar_quantum_key_distribution.retrieve_key(destination_keypair)
+
+  print(f"Retrieved key: {retrieved_key}")
