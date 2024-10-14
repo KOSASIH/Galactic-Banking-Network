f01b7e16483de03b218059e6529e5faa8e6b305c
KOSASIH <kosasihg88@gmail.com>
2024-10-14 13:48:28 +0700

    Create transaction_utils.py

---
+++ interdimensional_transaction_routing/utils/transaction_utils.py
@@ -0,0 +1,14 @@
+# transaction_utils.py
+
+def calculate_transaction_latency(transaction):
+    """
+    Calculate the latency of a transaction.
+
+    Parameters:
+    - transaction: Transaction to calculate latency for.
+
+    Returns:
+    - latency: Latency of the transaction.
+    """
+    # Implement transaction latency calculation algorithm here
+    return len(transaction)
