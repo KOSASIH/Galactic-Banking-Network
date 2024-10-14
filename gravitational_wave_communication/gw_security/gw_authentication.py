df0912cb190f0957026cdefd27af60b0d14a8722
KOSASIH <kosasihg88@gmail.com>
2024-10-14 21:35:26 +0700

    Update gw_authentication.py

--- gravitational_wave_communication/gw_security/gw_authentication.py
+++ gravitational_wave_communication/gw_security/gw_authentication.py
@@ -1,14 +1,84 @@
-# gw_authentication.py
-
-import gw_cryptography
+import hashlib
+import hmac
+import base64
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import hashes
+from cryptography.x509 import load_pem_x509_certificate
+from cryptography.x509.oid import NameOID
+import os
+import binascii
+import time
+import random
+import string
+import json
+import jwt
+import datetime
+from datetime import timedelta
 
 class GWAuthentication:
-    def __init__(self, authentication_key):
-        self.authentication_key = authentication_key
+    def __init__(self, private_key, public_key):
+        self.private_key = serialization.load_pem_private_key(
+            private_key,
+            password=None,
+            backend=default_backend()
+        )
+        self.public_key = serialization.load_pem_public_key(
+            public_key,
+            backend=default_backend()
+        )
 
-    def authenticate_node(self, node_id, authentication_data):
-        # Authenticate a node using a cryptographic key
-        expected_data = gw_cryptography.encrypt_signal(node_id.encode(), self.authentication_key)
-        if authentication_data == expected_data:
-            return True
-        return False
+    def generate_token(self, user_id, expires_in=3600):
+        payload = {
+            'user_id': user_id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=expires_in)
+        }
+        return jwt.encode(payload, self.private_key, algorithm='RS256')
+
+    def verify_token(self, token):
+        try:
+            payload = jwt.decode(token, self.public_key, algorithms=['RS256'])
+            return payload['user_id']
+        except jwt.ExpiredSignatureError:
+            return None
+        except jwt.InvalidTokenError:
+            return None
+
+    def encrypt_data(self, data):
+        cipher = Cipher(algorithms.AES(self.private_key), modes.CBC(b'\0' * 16), backend=default_backend())
+        encryptor = cipher.encryptor()
+        padder = padding.PKCS7(128).padder()
+        padded_data = padder.update(data) + padder.finalize()
+        return encryptor.update(padded_data) + encryptor.finalize()
+
+    def decrypt_data(self, encrypted_data):
+        cipher = Cipher(algorithms.AES(self.private_key), modes.CBC(b'\0' * 16), backend=default_backend())
+        decryptor = cipher.decryptor()
+        decrypted_padded_data = decryptor.update(encrypted_data) + decryptor.finalize()
+        unpadder = padding.PKCS7(128).unpadder()
+        return unpadder.update(decrypted_padded_data) + unpadder.finalize()
+
+def main():
+    private_key = open('private_key.pem', 'rb').read()
+    public_key = open('public_key.pem', 'rb').read()
+    gw_auth = GWAuthentication(private_key, public_key)
+
+    user_id = 'user123'
+    token = gw_auth.generate_token(user_id)
+    print('Generated Token:', token)
+
+    verified_user_id = gw_auth.verify_token(token)
+    print('Verified User ID:', verified_user_id)
+
+    data = b'Hello, World!'
+    encrypted_data = gw_auth.encrypt_data(data)
+    print('Encrypted Data:', encrypted_data)
+
+    decrypted_data = gw_auth.decrypt_data(encrypted_data)
+    print('Decrypted Data:', decrypted_data)
+
+if __name__ == '__main__':
+    main()
