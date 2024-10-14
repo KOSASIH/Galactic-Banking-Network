8a10c055e1374e829952842b93a1a2a11f066dcb
KOSASIH <kosasihg88@gmail.com>
2024-10-14 13:35:13 +0700

    Create cryptocurrency_utils.py

---
+++ exotic_matter_cryptocurrency_mining/utils/cryptocurrency_utils.py
@@ -0,0 +1,14 @@
+# cryptocurrency_utils.py
+
+def calculate_block_reward(block_data):
+    """
+    Calculate the block reward.
+
+    Parameters:
+    - block_data: Block data.
+
+    Returns:
+    - block_reward: Block reward.
+    """
+    # Implement block reward calculation algorithm here
+    return len(block_data)
