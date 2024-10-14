df851b8e3ad7c223c07684c5a46e435f9b73800b
KOSASIH <kosasihg88@gmail.com>
2024-10-14 13:36:26 +0700

    Create config.py

---
+++ exotic_matter_cryptocurrency_mining/config.py
@@ -0,0 +1,8 @@
+# config.py
+
+class Config:
+    def __init__(self):
+        self.exotic_matter_generator_energy_output = 1000
+        self.exotic_matter_storage_capacity = 100
+        self.mining_pool_size = 10
+        self.block_reward = 100
