2ddc27c10dd41b4b47c8595ba3b8c566d8209892
KOSASIH <kosasihg88@gmail.com>
2024-10-17 16:26:45 +0700

    Create index.js

---
+++ vr-banking-interface/src/index.js
@@ -0,0 +1,13 @@
+// src/index.js
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './styles.css';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
